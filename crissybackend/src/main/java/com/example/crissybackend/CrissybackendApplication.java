3270aa136748ca73b71be68008742e114f39f2b1
Jasmine Lim <77251890+jasminelim327@users.noreply.github.com>
2023-06-10 13:48:50 +0800

    Add files via upload

---
+++ crissybackend/src/main/java/com/example/crissybackend/CrissybackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.crissybackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrissybackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CrissybackendApplication.class, args);
+    }
+
+}
